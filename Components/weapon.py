a5cb8b786977d76e8fda2993010631700679b494
Atanas Hristov <the_doktora@abv.bg>
2024-02-17 23:47:41 +0200

    Refactoring.

--- Components/weapon.py
+++ Components/weapon.py
@@ -1,7 +1,6 @@
 from item import Item
 
 class Weapon(Item):
-    def __init__(self, name, damage):
-        super().__init__(name)
+    def __init__(self, name, position, damage):
+        super().__init__(name, position)
         self.damage = damage
-        
