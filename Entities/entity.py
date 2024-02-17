2453e83cf30cd39522b30163176e6f42a1e03344
Atanas Hristov <the_doktora@abv.bg>
2024-02-17 23:47:23 +0200

    Added speed, surf, rect as arguments. Added drop() function.

--- Entities/entity.py
+++ Entities/entity.py
@@ -1,17 +1,23 @@
 class Entity:
-    def __init__(self, name, health, position):
+    def __init__(self, name, health, position, speed, surf, rect):
         self.name = name
         self.health = health
         self.position = position
-        
+        self.speed = speed
+        self.surf = surf
+        self.rect = rect
+    
     def alive(self):
         return self.health > 0
     
     def move(self, movement_vector):
         self.position = tuple(map(sum, zip(self.position, movement_vector)))
-        
+    
     def attack(self, target):
         pass
     
     def take_damage(self, damage):
         pass
+    
+    def drop(self, item):
+        item.position = tuple(self.position)
