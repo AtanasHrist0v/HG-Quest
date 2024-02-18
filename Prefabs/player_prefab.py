77abe5b22006725cb5a73ac7614249f80e6c8172
Atanas Hristov <the_doktora@abv.bg>
2024-02-18 06:14:17 +0200

    Created a prefab for the player's character.

---
+++ Prefabs/player_prefab.py
@@ -0,0 +1,17 @@
+import pygame
+from constants import DISPLAY_CENTER
+from Entities.player import Player
+from Prefabs.excalibur_prefab import excalibur
+
+player_surf = pygame.image.load('Assets/player.png').convert_alpha()
+player_rect = player_surf.get_rect(center = DISPLAY_CENTER)
+player = Player('Player', 
+                Player.MAX_HEALTH, 
+                pygame.Vector2(DISPLAY_CENTER), 
+                300, 
+                player_surf, 
+                player_rect, 
+                0, 
+                excalibur, 
+                None, 
+                [])
