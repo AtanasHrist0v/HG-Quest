a2ce7da4082a7989daf54952c310c54ccb45d9ec
Atanas Hristov <the_doktora@abv.bg>
2024-02-18 06:39:08 +0200

    Refactoring.

--- Prefabs/excalibur_prefab.py
+++ Prefabs/excalibur_prefab.py
@@ -1,7 +1,8 @@
 import pygame
+from constants import DISPLAY_CENTER
 from Components.weapon import Weapon
 
 
 excalibur_surf = pygame.image.load('Assets/phat_excalibur.png').convert_alpha()
-excalibur_rect = excalibur_surf.get_rect(center = screen_center)
-excalibur = Weapon('Excalibur', excalibur_surf, excalibur_rect, 420, pygame.Rect(screen_center, (100, 100)), 50)
+excalibur_rect = excalibur_surf.get_rect(center = DISPLAY_CENTER)
+excalibur = Weapon('Excalibur', excalibur_surf, excalibur_rect, 420, pygame.Rect(DISPLAY_CENTER, (100, 100)), 50)
